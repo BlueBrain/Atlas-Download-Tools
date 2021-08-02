f9d2365adf2bdb3a17405dc1b5ea24286c13838b
Emilie Delattre <emilie.delattre@epfl.ch>
2021-08-02 15:26:14 +0200

    Try to fix documentation build

--- docs/conf.py
+++ docs/conf.py
@@ -58,7 +58,7 @@ version = atldld.__version__
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = []
 
 # Do not mention module names
 add_module_names = False
