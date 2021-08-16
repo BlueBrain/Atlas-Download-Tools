b394a4e266537187fdd72e2f7ce93e0b2f435b0c
Stanislav Schmidt <stanislav.schmidt@epfl.ch>
2021-08-16 11:08:28 +0200

    Add the atldld/__main__.py file

---
+++ src/atldld/__main__.py
@@ -0,0 +1,29 @@
+# The package atldld is a tool to download atlas data.
+#
+# Copyright (C) 2021 EPFL/Blue Brain Project
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""The main CLI entry point.
+
+This module enables running the CLI by running `atldld` as a module:
+
+    $ python -m atldld
+
+This way it is also possible to debug the CLI using the PDB debugger:
+
+    $ python -m pdb -m atldld
+"""
+from atldld.cli import root
+
+root()
