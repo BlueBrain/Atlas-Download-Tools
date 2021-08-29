82886dcb95469c2bd5f5b3215e685e8534684f75
Stanislav Schmidt <stanislav.schmidt@epfl.ch>
2021-08-29 19:40:46 +0200

    Move find_3d_affine to the new maths module

--- src/atldld/cli.py
+++ src/atldld/cli.py
@@ -290,61 +290,9 @@ def download_faithful_dataset(dataset_id, output_dir):
     click.secho(f"{volume_path.resolve().as_uri()}", fg="yellow", bold=True)
 
 
-from itertools import combinations
-
 import numpy as np
 from scipy import ndimage
 from skimage.color import rgb2gray
-
-
-def find_3d_affine(p_from, p_to):
-    """Find a 3D shearless affine transformation given mapping of 3 points.
-
-    With the assumption of no shear we can find the 4th point and its mapping
-    by the cross product. For example `p_from = (p1, p2, p3)` gives two vectors
-    `v1 = p2 - p1` and `v2 = p3 - p1`, giving `v3 = v1 x v2`. The fourth
-    point is then given by `p4 = p1 + v3`.
-
-    The only caveat is the length of the vector `v3`. We must make sure that it
-    scales in the same way between `p_from` and `p_to` as all other points. The
-    easiest way to ensure this is to give it the same length as the `v1` vector:
-    `v3 = |v1| * (v1 x v2 / |v1| / |v2|) = v1 x v2 / |v2|`.
-    """
-
-    # TODO
-    # Some sanity checks on the input data: make sure the two triangles
-    # formed by the input points have the same shape and orientation
-
-    def add_fourth(points):
-        v1 = points[1] - points[0]
-        v2 = points[2] - points[0]
-        v3 = np.cross(v1, v2) / np.linalg.norm(v2)
-        p4 = points[0] + v3
-
-        return np.stack([*points, p4])
-
-    p_from = add_fourth(p_from)
-    p_to = add_fourth(p_to)
-
-    # check uniform scaling
-    scales = []
-    tolerance = 0.02
-    for i, j in combinations(range(3), 2):
-        len_from = np.linalg.norm(p_from[i] - p_from[j])
-        len_to = np.linalg.norm(p_to[i] - p_to[j])
-        scales.append(len_from / len_to)
-    #     print(scales)
-    #     print(np.all([abs(s1 - s2) / min(s1, s2) < tolerance for s1, s2 in combinations(scales, 2)]))
-
-    # Add homogenous coordinate and transpose so that
-    # - dim_0 = "xyz1"
-    # - dim_1 = points
-    p_from = np.concatenate([p_from.T, np.array([[1, 1, 1, 1]])])
-    p_to = np.concatenate([p_to.T, np.array([[1, 1, 1, 1]])])
-
-    # Compute the affine transform so that p_to = A @ p_from
-    # => A = p_to @ p_from_inv
-    return p_to @ np.linalg.inv(p_from)
 
 
 def get_bbox(points):
@@ -374,6 +322,8 @@ def bbox_meshgrid(bbox):
 
 
 def get_true_ref_image(image, corners, slice_width_um=25):
+    from atldld.maths import find_shearless_3d_affine
+
     # skip image download and corner queries because it would take too long.
     # instead pre-compute them and take them as parameters for now
     # image = aibs.get_image(image_id)
@@ -390,7 +340,7 @@ def get_true_ref_image(image, corners, slice_width_um=25):
         (nx, ny, 0),
     ])
     p_from = corners[:3]
-    affine = find_3d_affine(p_from, p_to)
+    affine = find_shearless_3d_affine(p_from, p_to)
 
     # Create the meshgrid
     out_bbox = get_bbox(corners)
