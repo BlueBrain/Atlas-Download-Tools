76e5ee8ce67855ce6828bfc147ef61e4b5f990d4
Stanislav Schmidt <stanislav.schmidt@epfl.ch>
2021-10-11 09:54:46 +0200

    Add all values to PlaneOfSection
    This is necessary for the search dataset CLI command as any
    of the values can be part of the search results

--- tests/test_dataset.py
+++ tests/test_dataset.py
@@ -21,14 +21,50 @@ class TestPlaneOfSection:
     def test_members(self):
         assert "CORONAL" in PlaneOfSection.__members__
         assert "SAGITTAL" in PlaneOfSection.__members__
+        assert "VARIABLE" in PlaneOfSection.__members__
+        assert "NA" in PlaneOfSection.__members__
+        assert "TRANSVERSE" in PlaneOfSection.__members__
+        assert "LATERAL_TO_MEDIAL" in PlaneOfSection.__members__
+        assert "MEDIAL_TO_LATERAL" in PlaneOfSection.__members__
+        assert "VENTRAL_TO_DORSAL" in PlaneOfSection.__members__
+        assert "DORSAL_TO_VENTRAL" in PlaneOfSection.__members__
+        assert "CORONAL_CAUDAL_TO_ROSTRAL" in PlaneOfSection.__members__
+        assert "CORONAL_ROSTRAL_TO_CAUDAL" in PlaneOfSection.__members__
+        assert "TANGENTIAL" in PlaneOfSection.__members__
 
     def test_values(self):
         assert PlaneOfSection.CORONAL.value == 1
         assert PlaneOfSection.SAGITTAL.value == 2
+        assert PlaneOfSection.VARIABLE.value == 4
+        assert PlaneOfSection.NA.value == 5
+        assert PlaneOfSection.TRANSVERSE.value == 6
+        assert PlaneOfSection.LATERAL_TO_MEDIAL.value == 7
+        assert PlaneOfSection.MEDIAL_TO_LATERAL.value == 8
+        assert PlaneOfSection.VENTRAL_TO_DORSAL.value == 9
+        assert PlaneOfSection.DORSAL_TO_VENTRAL.value == 10
+        assert PlaneOfSection.CORONAL_CAUDAL_TO_ROSTRAL.value == 11
+        assert PlaneOfSection.CORONAL_ROSTRAL_TO_CAUDAL.value == 531107579
+        assert PlaneOfSection.TANGENTIAL.value == 1082279372
 
     def test_str(self):
         assert str(PlaneOfSection.CORONAL) == "coronal"
         assert str(PlaneOfSection.SAGITTAL) == "sagittal"
+        assert str(PlaneOfSection.VARIABLE) == "variable"
+        assert str(PlaneOfSection.NA) == "n/a"
+        assert str(PlaneOfSection.TRANSVERSE) == "transverse"
+        assert str(PlaneOfSection.LATERAL_TO_MEDIAL) == "Lateral to Medial"
+        assert str(PlaneOfSection.MEDIAL_TO_LATERAL) == "Medial to Lateral"
+        assert str(PlaneOfSection.VENTRAL_TO_DORSAL) == "Ventral to Dorsal"
+        assert str(PlaneOfSection.DORSAL_TO_VENTRAL) == "Dorsal to Ventral"
+        assert (
+            str(PlaneOfSection.CORONAL_CAUDAL_TO_ROSTRAL)
+            == "Coronal (Caudal to Rostral)"
+        )
+        assert (
+            str(PlaneOfSection.CORONAL_ROSTRAL_TO_CAUDAL)
+            == "Coronal (Rostral to Caudal)"
+        )
+        assert str(PlaneOfSection.TANGENTIAL) == "Tangential"
 
 
 class TestReferenceSpace:
