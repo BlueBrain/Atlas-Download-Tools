213fb087d65d106e4b81d63bb120ccb4ba54e42e
Stanislav Schmidt <stanislav.schmidt@epfl.ch>
2021-10-11 11:04:27 +0200

    Implemented the atldld search dataset command

--- src/atldld/cli/search.py
+++ src/atldld/cli/search.py
@@ -24,8 +24,70 @@ def search_cmd():
 
 
 @search_cmd.command("dataset", help="Search datasets")
-def search_dataset_cmd():
+@click.option("-i", "--id", "dataset_id", help="The dataset ID")
+@click.option("-g", "--gene-name", "gene_acronym", help="The gene acronym")
+@click.option("-s", "--specimen", "specimen_id", help="The specimen ID")
+@click.option(
+    "-p",
+    "--plane-of-section",
+    "plane_of_section",
+    help="The plane of section, e.g. coronal, sagittal, etc.",
+)
+def search_dataset_cmd(dataset_id, specimen_id, gene_acronym, plane_of_section):
     """Run search subcommand."""
+    from atldld import requests
+    from atldld.dataset import PlaneOfSection
+
+    criteria = {}
+    if dataset_id is not None:
+        criteria["id"] = dataset_id
+    if specimen_id is not None:
+        criteria["specimen_id"] = specimen_id
+    if gene_acronym is not None:
+        criteria["genes"] = {"acronym": gene_acronym}
+    if plane_of_section is not None:
+        criteria["plane_of_section"] = {"name": plane_of_section}
+
+        if plane_of_section not in PlaneOfSection.__names__:
+            click.secho(
+                f'Unknown plane of section name: "{plane_of_section}"', fg="yellow"
+            )
+            click.secho("Probably no datasets will be found.", fg="yellow")
+            click.secho("Valid plane of section names are:", fg="yellow")
+            for name in PlaneOfSection.__names__.values():
+                click.secho(f"* {name}", fg="yellow")
+
+    if len(criteria) == 0:
+        raise click.ClickException(
+            "At least one of the search criteria has to be specified. "
+            "Use the --help flag to see all available criteria."
+        )
+
+    # Send request
+    rma_parameters = requests.RMAParameters(
+        "SectionDataSet",
+        criteria=criteria,
+        include=["genes", "section_images"],
+    )
+    click.secho("Searching...", fg="green")
+    try:
+        msg = requests.rma_all(rma_parameters)
+    except requests.RMAError as exc:
+        raise click.ClickException(
+            f"An error occurred while querying the AIBS servers: {str(exc)}"
+        )
+
+    if len(msg) == 0:
+        click.secho("No datasets found", fg="red")
+    else:
+        click.secho(f"{len(msg)} dataset(s) found:", fg="green")
+        for meta in msg:
+            genes = ", ".join(gene["acronym"] for gene in meta["genes"])
+            plane_of_section = PlaneOfSection(meta["plane_of_section_id"])
+            print(
+                f"* id: {meta['id']:10d}, genes: {genes:>10s}, "
+                f"{plane_of_section}, {len(meta['section_images'])} section images"
+            )
 
 
 @search_cmd.command("img", help="Search section images")
